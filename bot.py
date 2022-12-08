6170be6f66509b1f481fbba7ebf6a839825e3568
Ams <sasha_ams@mail.ru>
2022-12-08 22:06:05 +0500

    телеграм эхо-бот выводит стикер приветсвие v1.1

--- bot.py
+++ bot.py
@@ -1,8 +1,17 @@
+from ast import parse
 import telebot
 
 import config
 
 bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('Pictures//welcome.mp4', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    bot.send_message(message.chat.id, 'Добро пожаловать, {0.first_name}!\nЯ - {1.first_name}</b>, бот созданный чтобы на мне учились'.format(message.from_user, bot.get_me()),
+    parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
